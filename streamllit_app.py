fed808b0d621f23daccb0da4f0e5d5b3b2f4e194
olivejones <141438116+olivejones@users.noreply.github.com>
2023-08-08 12:22:39 +0100

    Update streamllit_app.py
    added text 
    added images

--- streamllit_app.py
+++ streamllit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title ('My Parents New Healthy Diner')
 streamlit.header ('Breakfast Menu')
-streamlit.text ('Omega 3 & blueberry oatmeal')
-streamlit.text ('Kale, Spinach,Rocket smoothie')
-streamlit.text ('Hard-Boiled Free- Range Eggs')
+streamlit.text (' 🥣  Omega 3 & blueberry oatmeal')
+streamlit.text (' 🥗 Kale, Spinach,Rocket smoothie')
+streamlit.text (' 🐔 Hard-Boiled Free- Range Eggs')
+streamlit.text (' 🥑🍞Avacado Toast')
